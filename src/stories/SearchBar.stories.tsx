6f5d7742182550cd4960424169b2c192d81bdaaa
Alberto Crocini <acrocini@florence-consulting.it>
2023-02-14 15:06:42 +0100

    Added BreadCrumbs + SearchBar + Icons

---
+++ src/stories/SearchBar.stories.tsx
@@ -0,0 +1,30 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+
+import { SearchBar } from "../components/SearchBar";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/SearchBar",
+  component: SearchBar,
+  decorators: [
+    (Story) => (
+      <div style={{ maxWidth: 300 }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof SearchBar>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof SearchBar> = (args) => <SearchBar {...args} />;
+
+const defaultArgs = {
+  defaultValue: "Default Value",
+};
+
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  ...defaultArgs,
+};
