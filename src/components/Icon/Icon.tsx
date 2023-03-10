fdb41b528bc24290e68475ecb77227dec36ef6b7
Niccolò Taddei <ntaddei@florence-consulting.it>
2023-03-10 15:07:42 +0100

    Fix Icon display style

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -58,6 +58,7 @@ const getColor = (color: IconProps["color"]) => css`
 `;
 
 const IconRoot = styled.span<Omit<IconProps, "name" | "color">>`
+  display: inline-flex;
   ${(props) => getColor(props.color)};
 
   > svg {
