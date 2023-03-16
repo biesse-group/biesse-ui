1c961294546d79aa28e9616fc81f0c2c19ff4c1e
Niccolò Taddei <ntaddei@florence-consulting.it>
2023-03-16 10:42:27 +0100

    Fix product carousel for various screen dimensions

--- src/components/ProductCarousel/ProductCarousel.tsx
+++ src/components/ProductCarousel/ProductCarousel.tsx
@@ -1,6 +1,4 @@
-import { AnimatePresence, HTMLMotionProps, motion, wrap } from "framer-motion";
-import { useMemo } from "react";
-import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 import styled, { css } from "styled-components";
 
 import { mqUntil } from "../../styles/media-queries";
@@ -8,11 +6,12 @@ import { getKeys } from "../../utils/getKeys";
 import { Title } from "../Title";
 import { BackgroundStrip } from "./BackgroundStrip";
 import { ControlButton } from "./ControlButton";
+import { ItemDetail } from "./ItemDetail";
 import { ItemImage } from "./ItemImage";
-import { ItemPosition, ItemProps } from "./itemProps";
 import { ItemsStrip } from "./ItemsStrip";
 import { ItemTitle } from "./ItemTitle";
-import { detailVariants, imageVariants, mobileVariants, titleVariants } from "./variants";
+import { useProductCarousel } from "./useProductCarousel";
+import { imageVariants, mobileVariants, titleVariants } from "./variants";
 
 const CarouselTitle = styled(Title)`
   font-size: ${(props) => props.theme.font.regular.headings.xxl};
@@ -30,19 +29,6 @@ const CarouselTitle = styled(Title)`
 const ItemDetailWrapper = styled.div`
   display: flex;
   justify-content: center;
-`;
-
-const ItemDetail = styled(motion.div)`
-  width: 100%;
-  max-width: 600px;
-  position: absolute;
-
-  ${mqUntil(
-    "sm",
-    css`
-      padding: 0 30px;
-    `
-  )}
 `;
 
 export type ProductCarouselProps<T extends object> = {
@@ -86,54 +72,8 @@ export const ProductCarousel = <T extends object>({
   renderDetail,
   renderImage,
 }: ProductCarouselProps<T>) => {
-  const adaptedItems = useMemo(() => [...items, ...items, ...items], [items]);
-  const [[page, direction], setPage] = useState([items.length, 0]);
-  const [locks, setLocks] = useState(0);
-
-  const currIndex = wrap(0, adaptedItems.length, page);
-  const prevIndex = wrap(0, adaptedItems.length, page - 1);
-  const nextIndex = wrap(0, adaptedItems.length, page + 1);
-
-  const shownIndex = {
-    left: prevIndex,
-    center: currIndex,
-    right: nextIndex,
-  };
-
-  const handleChange = (newDirection: number) => {
-    if (!locks) {
-      setPage([page + newDirection, newDirection]);
-    }
-  };
-
-  const lock = () => {
-    setLocks(locks + 1);
-  };
-
-  const unlock = () => {
-    if (locks > 0) {
-      setLocks(locks - 1);
-    }
-  };
-
-  const getItemMotionProps = (
-    pos: ItemPosition,
-    { isMobile }: { isMobile?: boolean } = {}
-  ): HTMLMotionProps<"div"> & ItemProps & { key: React.Key } => ({
-    position: pos,
-    key: isMobile ? `mobile-${currIndex}` : shownIndex[pos],
-    initial: "initial",
-    animate: "animate",
-    exit: "exit",
-    custom: direction,
-    transition: {
-      type: "tween",
-      ease: [0.09, 0.28, 0.45, 0.95],
-      duration: 0.5,
-    },
-    onAnimationStart: lock,
-    onAnimationComplete: unlock,
-  });
+  const { direction, page, nextPage, prevPage, getItemMotionProps, shownItems } =
+    useProductCarousel(items);
 
   return (
     <div style={{ position: "relative", height: 500 + contentHeight }}>
@@ -142,55 +82,49 @@ export const ProductCarousel = <T extends object>({
           {title}
         </CarouselTitle>
 
-        {/* DESKTOP CAROUSEL */}
+        {/* Desktop carousel */}
         <ItemsStrip>
           <AnimatePresence initial={false} custom={direction}>
-            {getKeys(shownIndex).map((pos) => (
+            {getKeys(shownItems).map((pos) => (
               <ItemTitle variants={titleVariants[pos]} {...getItemMotionProps(pos)}>
-                {renderTitle(adaptedItems[shownIndex[pos]])}
+                {renderTitle(shownItems[pos])}
               </ItemTitle>
             ))}
           </AnimatePresence>
         </ItemsStrip>
         <ItemsStrip>
           <AnimatePresence initial={false} custom={direction}>
-            {getKeys(shownIndex).map((pos) => (
+            {getKeys(shownItems).map((pos) => (
               <ItemImage variants={imageVariants[pos]} {...getItemMotionProps(pos)}>
-                {renderImage(adaptedItems[shownIndex[pos]])}
+                {renderImage(shownItems[pos])}
               </ItemImage>
             ))}
           </AnimatePresence>
         </ItemsStrip>
 
-        {/* MOBILE CAROUSEL */}
+        {/* Mobile carousel */}
         <ItemsStrip isMobile>
           <AnimatePresence initial={false} custom={direction}>
             <ItemTitle variants={mobileVariants} {...getItemMotionProps("center")}>
-              {renderTitle(adaptedItems[currIndex])}
+              {renderTitle(shownItems["center"])}
             </ItemTitle>
           </AnimatePresence>
         </ItemsStrip>
         <ItemsStrip isMobile>
           <AnimatePresence initial={false} custom={direction}>
             <ItemImage variants={mobileVariants} {...getItemMotionProps("center")}>
-              {renderImage(adaptedItems[currIndex])}
+              {renderImage(shownItems["center"])}
             </ItemImage>
           </AnimatePresence>
         </ItemsStrip>
-        <ControlButton direction="prev" onClick={() => handleChange(-1)} />
-        <ControlButton direction="next" onClick={() => handleChange(1)} />
+
+        {/* Control buttons */}
+        <ControlButton direction="prev" onClick={nextPage} />
+        <ControlButton direction="next" onClick={prevPage} />
       </BackgroundStrip>
       <ItemDetailWrapper>
         <AnimatePresence initial={false}>
-          <ItemDetail
-            variants={detailVariants}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            key={page}
-          >
-            {renderDetail(adaptedItems[currIndex])}
-          </ItemDetail>
+          <ItemDetail key={page}>{renderDetail(shownItems["center"])}</ItemDetail>
         </AnimatePresence>
       </ItemDetailWrapper>
     </div>
