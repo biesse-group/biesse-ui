be1fa243702e808dd0ffe85c602070524314fcb1
Niccolò Taddei <ntaddei@florence-consulting.it>
2023-04-04 08:18:55 +0200

    Add quote component

---
+++ src/stories/Quote.stories.tsx
@@ -0,0 +1,23 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Quote } from "../components";
+
+export default {
+  title: "components/Quote",
+  component: Quote,
+  parameters: {
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof Quote>;
+
+const Template: ComponentStory<typeof Quote> = (args) => <Quote {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  citation:
+    "Il nuovo mandrino ES951 e la piattaforma My HSD sono un connubio ad alta performance perché andiamo ad unire quella che è la performance del mandrino alla possibilità di andare a rilevare tutti i dati che questo può trasmettere a distanza, dando la possibilità di monitorare in tempo reale tutto quello che sta succedendo sul mandrino",
+  author: {
+    name: "John Doe",
+    description: "UTE/Service Manager A.C.M.E. spa",
+  },
+};
