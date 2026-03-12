309a1a5d5a7ca1041dbb35bc3ebad647bec66b51
FCG\aleandri <aleandri@florence-consulting.it>
2026-03-12 12:15:19 +0100

    added new logo & create eslintignore

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,10 @@
 module.exports = {
   testEnvironment: "jsdom",
   moduleNameMapper: {
-    ".(css|less|scss)$": "identity-obj-proxy",
-    ".(ttf|eot|woff|woff2)$": "identity-obj-proxy",
-    ".(mdx)$": "identity-obj-proxy",
+    "\\.(css|less|scss)$": "identity-obj-proxy",
+    "\\.(mdx)$": "identity-obj-proxy",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$":
+      "<rootDir>/__mocks__/fileMock.js",
     "^~(.*)$": "<rootDir>/src/$1",
   },
 };
